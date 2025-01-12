6477f941f179bbb5ec4f0263f34716601e745de3
GovLynk <admin@govlynk.com>
2025-01-11 22:46:47 -0600

    updated SAM API Key

--- amplify/functions/samApi/resource.ts
+++ amplify/functions/samApi/resource.ts
@@ -3,7 +3,7 @@ import { defineFunction, secret } from "@aws-amplify/backend";
 export const samApi = defineFunction({
 	name: "samApi",
 	environment: {
-		SAM_API_KEY: secret("SAM_API_KEY"),
+		SAM_API_KEY: secret("l5nES3FOs2N3Z1Uzd2IYck2eQn6xETUyfGnNPoEl"),
 	},
 	entry: "./handler.ts",
 });
