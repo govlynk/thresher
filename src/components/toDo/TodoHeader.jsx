d9926e045224ed487ce0b990506fde4e1b07d046
GovLynk <admin@govlynk.com>
2025-01-29 09:34:38 -0600

    added missing file

--- src/components/toDo/TodoHeader.jsx
+++ src/components/toDo/TodoHeader.jsx
@@ -20,13 +20,13 @@ export function TodoHeader({ onAddClick, onSprintClick, onClose }) {
 				return;
 			}
 
-			if (!sprints || sprints.length === 0) {
-				try {
-					await generateSprints(activeTeamId);
-				} catch (err) {
-					console.error("Error generating sprints:", err);
-				}
-			}
+			// if (!sprints || sprints.length === 0) {
+			// 	try {
+			// 		await generateSprints(activeTeamId);
+			// 	} catch (err) {
+			// 		console.error("Error generating sprints:", err);
+			// 	}
+			// }
 		};
 
 		initializeSprints();
