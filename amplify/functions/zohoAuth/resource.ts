d3e7a6317fa7a4acfcd2bf0c93cf4bdd84135f01
GovLynk <admin@govlynk.com>
2025-02-22 15:34:49 -0600

    updating callback uri

--- amplify/functions/zohoAuth/resource.ts
+++ amplify/functions/zohoAuth/resource.ts
@@ -6,7 +6,7 @@ export const zohoAuth = defineFunction({
 	runtime: 18,
 	memoryMB: 2048,
 	environment: {
-		REDIRECT_URI: "https://member.govlynk.com/auth/zoho/callback",
+		REDIRECT_URI: "https://d1hcn7nmh82rdz.execute-api.us-east-1.amazonaws.com/dev/zoho/callback",
 		ZOHO_CLIENT_ID: secret("ZOHO_CLIENT_ID"),
 		ZOHO_CLIENT_SECRET: secret("ZOHO_CLIENT_SECRET"),
 		ZOHO_REFRESH_TOKEN: secret("ZOHO_REFRESH_TOKEN"),
